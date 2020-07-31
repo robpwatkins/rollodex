4a71b1f1c141f81fc3e0ef8aebfde4461ba9de25
Rob W <robpwatkins@gmail.com>
2020-07-30 20:48:13 -0500

    getting it

--- src/PuppyPage.js
+++ src/PuppyPage.js
@@ -18,7 +18,7 @@ class PeoplePage extends React.Component {
     // const peopleUrl = 'https://randomuser.me/api?results=25';
     // const puppyUrl = 'https://gist.githubusercontent.com/robpwatkins/3fddbada918e592e3901a474b0953df9/raw/d7fb568d2bdcb69504249a6a3c80091723ebc20f/puppyAPI.json';
     // const starWarUrl = 'https://swapi.dev/api/planets' 
-    const puppyAPI = 'http://www.puppyapi.com/pups/'
+    // const puppyAPI = 'http://www.puppyapi.com/pups/'
     fetch(puppyAPI).then(response => {
       console.log(response)
       return response.json();
